94a404f96426e759e5acdadb52b67f91ffda648e
Joe Bryan Alanis <101160575+joe-bryan@users.noreply.github.com>
2023-09-01 07:34:36 -0500

    testing 2

--- main_flow.py
+++ main_flow.py
@@ -1,5 +1,5 @@
 from subway_locations import flow_live_locations_subway
-# from subway_schedule import get_gtfs_subway_schedule
+from subway_schedule import get_gtfs_subway_schedule
 # from subway_locations_schedules import subway_times
 # from write_bigquery_table import write_subways_to_bigquery
 from prefect import flow
@@ -12,11 +12,11 @@ def main_flow():
     # Get the live subway data
     live_location_subways = flow_live_locations_subway()
 
-    # # Get the schedules for today
-    # schedules_today = get_gtfs_subway_schedule(wait_for=[live_location_subways])
+    # Get the schedules for today
+    schedules_today = get_gtfs_subway_schedule()
 
     # # Compare live subways with schedules
-    # subway_times(wait_for=[schedules_today])
+    # subway_times(wait_for=[live_location_subways, schedules_today])
 
     # # Write any late subways to bigquery table
     # write_subways_to_bigquery(wait_for=[subway_times])
