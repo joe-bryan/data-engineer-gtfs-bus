6b0c1be995a353f4e7f1a9c57cdb6ad1acbae376
Joe Bryan Alanis <101160575+joe-bryan@users.noreply.github.com>
2023-09-09 13:37:21 -0500

    t

--- schedule.py
+++ schedule.py
@@ -19,12 +19,15 @@ def schedule_feed(schedule_url: str):
         routes = pd.read_csv(myzip.open("routes.txt"), low_memory=False)
         trip = pd.read_csv(myzip.open("trips.txt"), low_memory=False)
         calendar = pd.read_csv(myzip.open("calendar.txt"), low_memory=False)
-        # stop_times = pd.read_csv(myzip.open("stop_times.txt"), low_memory=False)
+        stop_times = pd.read_csv(
+            myzip.open("stop_times.txt"),
+            dtype={"trip_id": str, "stop_id": str, "stop_headsign": str},
+        )
         stops = pd.read_csv(myzip.open("stops.txt"), low_memory=False)
 
     # os.remove("MBTA_GTFS.zip")
 
-    return agency, routes, trip, calendar, stops
+    return agency, routes, trip, calendar, stop_times, stops
 
 
 # @task
